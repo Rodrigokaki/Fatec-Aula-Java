3a3a878bc4f387dd2171c35f8271820b3498cc0b
rodrigo.kakiuchi <rodrigo.kakiuchi2010@gmail.com>
2024-05-28 10:17:29 -0300

    Colocando status code de sucessos

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -1,8 +1,10 @@
 package com.fatec.student.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fatec.student.StudentService;
 import com.fatec.student.entities.Student;
@@ -23,28 +26,34 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping
-    public List<Student> getStudent(){
-        return studentService.getStudents();
+    public ResponseEntity<List<Student>> getStudent(){
+        return ResponseEntity.ok(studentService.getStudents());
     }
 
     @GetMapping("{id}")
-    public Student getStudentByStudent(@PathVariable int id){
-        return studentService.getStudentById(id);
+    public ResponseEntity<Student> getStudentByStudent(@PathVariable int id){
+        return ResponseEntity.ok(studentService.getStudentById(id));
     }
 
     @DeleteMapping("{id}")
-    public void deleteStudentById(@PathVariable int id){
+    public ResponseEntity<Void> deleteStudentById(@PathVariable int id){
         this.studentService.deleteStudentById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping
-    public void saveStudent(@RequestBody Student student){
-        this.studentService.saveStudent(student);
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        Student newStudent = this.studentService.saveStudent(student);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newStudent.getId()).toUri();
+
+        return ResponseEntity.created(location).body(newStudent);
     }
 
     @PutMapping("{id}")
-    public void updateStudent(@PathVariable int id, @RequestBody Student student){
+    public ResponseEntity<Void> updateStudent(@PathVariable int id, @RequestBody Student student){
         this.studentService.updateStudent(id, student);
+        return ResponseEntity.ok().build();
     }
 
 }
