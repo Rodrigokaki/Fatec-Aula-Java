7c553f9a6db1af7fd78b01be36adca416d8391b9
rodrigo.kakiuchi <rodrigo.kakiuchi2010@gmail.com>
2024-06-04 08:31:28 -0300

    Erros

---
+++ src/main/java/com/fatec/student/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.fatec.student.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandartError> entityNotFoundException(EntityNotFoundException e, HttpServletRequest request){
+
+        StandartError error = new StandartError();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("Resource Not Found");
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(Instant.now());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
