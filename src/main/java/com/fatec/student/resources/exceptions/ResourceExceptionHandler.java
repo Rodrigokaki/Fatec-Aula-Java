7441ebd42e777043d9c2a766251dfb4f45970883
rodrigo.kakiuchi <rodrigo.kakiuchi2010@gmail.com>
2024-06-11 10:46:53 -0300

    Mensagem de erro de validacao e status code

--- src/main/java/com/fatec/student/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fatec/student/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,4 +26,20 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrors> validationException(MethodArgumentNotValidException e, HttpServletRequest request){
+
+        ValidationErrors error = new ValidationErrors();
+        error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        error.setError("Validation Error");
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(Instant.now());
+        error.setPath(request.getRequestURI());
+
+        e.getBindingResult().getFieldErrors().forEach(ex -> error.addError(ex.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+    }
+
 }
