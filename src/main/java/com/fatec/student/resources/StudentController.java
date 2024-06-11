92663ce762cda5f87a8b18d9fbf6e63acf2ab9ba
rodrigo.kakiuchi <rodrigo.kakiuchi2010@gmail.com>
2024-06-11 09:13:14 -0300

    Validacoes

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,7 +44,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<StudentResponse> saveStudent(@RequestBody StudentRequest student){
+    public ResponseEntity<StudentResponse> saveStudent(@Validated @RequestBody StudentRequest student){
         StudentResponse newStudent = this.studentService.saveStudent(student);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newStudent.id()).toUri();
@@ -52,7 +53,7 @@ public class StudentController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Void> updateStudent(@PathVariable int id, @RequestBody StudentRequest student){
+    public ResponseEntity<Void> updateStudent(@PathVariable int id, @Validated @RequestBody StudentRequest student){
         this.studentService.updateStudent(id, student);
         return ResponseEntity.ok().build();
     }
