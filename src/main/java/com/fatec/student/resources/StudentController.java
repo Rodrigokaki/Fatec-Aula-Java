c9b716385428f06770a8a85d58e20e3fac36d4b6
rodrigo.kakiuchi <rodrigo.kakiuchi2010@gmail.com>
2024-06-11 11:06:17 -0300

    corrigindo put

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.fatec.student.dto.StudentRequest;
 import com.fatec.student.dto.StudentResponse;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:4200")
 @RequestMapping("students")
 public class StudentController {
     
